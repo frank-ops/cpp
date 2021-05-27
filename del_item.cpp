908f01013ab35fdd3780959de67d2b5acac5eb9d
frank-ops <57808379+frank-ops@users.noreply.github.com>
2021-05-27 09:47:12 +0530

    Update del_item.cpp

--- del_item.cpp
+++ del_item.cpp
@@ -2,21 +2,14 @@
 using namespace std;
 int main()
 {
-  int arr[100],n,item,i,count=0;
+  int n,item,count=0;
   cout<<"enter the length"<<" ";
   cin>>n;
-  if(100<n)
-  {
-    cout<<"size doesn't support";
-  }
-  else
-  {
-  cout<<"enter the elements";
+  int arr[n];
+  cout<<"enter the elements"<<" ";
   for(i=0;i<n;i++)
-   {
      cin>>arr[i];
-   }
-  cout<<"enter the elemwnt to delete"<<" ";
+  cout<<"enter the element to delete"<<" ";
   cin>>item;
   for(i=0;i<n;i++)
    {
@@ -29,18 +22,11 @@ int main()
   if(count)
   {
     for(int j=i;j<n-1;j++)
-    {
       arr[j]=arr[j+1];
-    }
-    cout<<"array after altering"<<"\n";
+    cout<<"array after deleting"<<"\n";
     for(i=0;i<n-1;i++)
-    {
       cout <<arr[i]<<" ";
-     }
-   }
+  }
    else
-   {
      cout<<"no such element to delete";
-   }
- }
 }
