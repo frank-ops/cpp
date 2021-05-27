e80ee819c652ec09dc34e1dba3aae4c2c78484c0
frank-ops <57808379+frank-ops@users.noreply.github.com>
2021-05-27 09:54:50 +0530

    Update del_item.cpp

--- del_item.cpp
+++ del_item.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 int main()
 {
-  int n,item,count=0;
-  cout<<"enter the length"<<" ";
-  cin>>n;
+  int n , item , count=0 , i ;
+  cout << "enter the length" << " " ;
+  cin >> n;
   int arr[n];
-  cout<<"enter the elements"<<" ";
-  for(i=0;i<n;i++)
-     cin>>arr[i];
-  cout<<"enter the element to delete"<<" ";
-  cin>>item;
-  for(i=0;i<n;i++)
+  cout << "enter the elements" << " " ;
+  for (i=0 ;i<n ;i++)
+     cin >> arr[i] ;
+  cout << "enter the element to delete" << " " ;
+  cin >> item ;
+  for (i=0 ;i<n ;i++)
    {
-     if(arr[i]==item)
+     if ( arr[i] == item )
      {
-       count++;
+       count++ ;
        break;
      }
    }
-  if(count)
+  if ( count )
   {
-    for(int j=i;j<n-1;j++)
-      arr[j]=arr[j+1];
-    cout<<"array after deleting"<<"\n";
-    for(i=0;i<n-1;i++)
-      cout <<arr[i]<<" ";
+    for (int j=i ;j<n-1 ;j++)
+      arr[j] = arr[j+1] ;
+    cout << "array after deleting" << "\n" ;
+    for (i=0 ;i<n-1 ;i++)
+      cout << arr[i] << " " ;
   }
    else
-     cout<<"no such element to delete";
+     cout << "no such element to delete" ;
 }
