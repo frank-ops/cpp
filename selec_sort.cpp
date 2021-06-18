cd8392acce5107bf703435d09b0f64afad0651f0
frank-ops <57808379+frank-ops@users.noreply.github.com>
2021-06-18 09:49:24 +0530

    Update selec_sort.cpp

--- selec_sort.cpp
+++ selec_sort.cpp
@@ -5,15 +5,15 @@ void selec_sort(int a[],int n)
   int mid;
   for(int i=0; i<n; i++)//loop to go through thr n elements in array
   {
-    mid=i;
+    min=i;
     for(int j=i+1;j<n;j++)
     {
-      if(a[j]<a[mid])//finds the least element in the array
+      if(a[j]<a[min])//finds the least element in the array
       {
-        mid=j;//takes mid to the index least element in array
+        min=j;//takes min to the index least element in array
       }
     }
-    swap(a[i],a[mid]);// swaps the least with the ith element in array
+    swap(a[i],a[min]);// swaps the least with the ith element in array
   }
 }
 int main()
